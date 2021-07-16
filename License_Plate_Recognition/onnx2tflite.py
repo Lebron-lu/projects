e7455e156d050517768752a8a36e34f3a351028b
luxian <luxian@rt-thread.com>
2021-07-16 15:14:58 +0800

    update something

--- License_Plate_Recognition/onnx2tflite.py
+++
@@ -1,9 +0,0 @@
-from onnx_tf.backend import prepare
-import onnx
-
-# onnx to pb
-TF_PATH = "./pbmodel"   # where the representation of tensorflow model will be stored
-ONNX_PATH = './lprnet.onnx'   # path to my existing ONNX model
-onnx_model = onnx.load(ONNX_PATH)  # load onnx model
-tf_rep = prepare(onnx_model)  # creating TensorflowRep object
-tf_rep.export_graph(TF_PATH)
